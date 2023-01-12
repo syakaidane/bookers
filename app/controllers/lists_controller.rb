aba53da7cc183131c9f31bd8d73bb6dcec654b18
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-01-12 08:37:04 +0000

    commit2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,24 +1,38 @@
 class ListsController < ApplicationController
-  def new
-    def new
-    @list = List.new  
-    end
     
-    def create
-    list = List.new(list_params)
-    list.save
-    redirect_to '/top'
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      @lists = List.all
+      render :index
     end
   end
 
   def index
+    @list = List.new
     @lists = List.all 
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
   
   private
